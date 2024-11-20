f3206c8ab2c932b551130c0417316d3066b3c1fa
ShowyaTanaka <shoyatanakatst@gmail.com>
2024-11-20 19:09:35 +0900

    Initial Commit

---
+++ FinderFileUtility 2/FinderFileUtility_2App.swift
@@ -0,0 +1,17 @@
+//
+//  FinderFileUtility_2App.swift
+//  FinderFileUtility 2
+//
+//  Created by Noel Light on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct FinderFileUtility_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinderFileUtility 2Tests/FinderFileUtility_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FinderFileUtility_2Tests.swift
+//  FinderFileUtility 2Tests
+//
+//  Created by Noel Light on 2024/11/20.
+//
+
+import Testing
+@testable import FinderFileUtility_2
+
+struct FinderFileUtility_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
