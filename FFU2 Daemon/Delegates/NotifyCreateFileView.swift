56e0d378538657d076c984ef09d001f385b1a331
ShowyaTanaka <showyatanakatst@gmail.com>
2025-12-21 23:08:18 +0900

    NSWindowの呼び出し処理を修正

--- FFU2 Daemon/Delegates/NotifyCreateFileView.swift
+++ FFU2 Daemon/Delegates/NotifyCreateFileView.swift
@@ -9,12 +9,16 @@ class NotifyCreateFileView: NSObject, CFMessagePortToNotificationHandlerDelegate
     var portName = "group.com.ShoyaTanaka.FFU2.editfile" as CFString
 
     @objc func callback(notification: Notification) {
+        print("AAAAAA")
         guard let object = notification.object as? [String: Any] else {return}
         guard let path = object["path"] as? String else {return}
         guard let selectedExtension = object["selected_extension"] as? String else {return}
 
         let viewModel = CreateFileViewModel(currentDirURL: URL(fileURLWithPath: path), selectedExtension: selectedExtension)
-        NSPanelService.createPanel(viewModel: viewModel, isfocused: true)
+        DispatchQueue.main.async {
+            NSPanelService.createPanel(viewModel: viewModel, isfocused: true)
+        }
+        
 
     }
 
